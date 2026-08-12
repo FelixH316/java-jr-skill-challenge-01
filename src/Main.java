604198042401bd7578407cb4cb58cc8f2c492f90
Felix Armenta <62727466+FelixH316@users.noreply.github.com>
2026-08-12 09:47:23 -0600

    feature/SCJ-0102 Main format

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,13 @@
-Main
+public class Main {
+    public static void main(String[] args) {
+        Perro perro1 = new Perro("Chucho", "perro",
+                "dorado", "caramelo",
+                "Medium");
+        perro1.hacerRuido();
+
+        Ave ave1 = new Ave("Chuy", "Perico",
+                "verde", "Triangulo");
+
+        ave1.hacerRuido();
+    }
+}
